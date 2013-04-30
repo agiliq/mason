04979237632dbab6a5ee005f16cdf349aad70906
Javed Khan <javed@agiliq.com>
2013-04-30 13:25:20 +0530

    fixed grappelli plugin; needs admin plugin disabled

--- mason/bricks/grappelli.py
+++ mason/bricks/grappelli.py
@@ -1,12 +1,12 @@
-from mason.bricks.base import BaseBrick
+from mason.bricks.admin import Admin
 
 
-class Grappelli(BaseBrick):
+class Grappelli(Admin):
 
     name = "Django Grappelli"
     description = "Configures Django Grappelli"
 
-    installed_apps = ['grappelli', 'django.contrib.admin',]
+    installed_apps = ['grappelli'] + Admin.installed_apps
     dependencies = ['django-grappelli', ]
 
-    urls = ["(r'^grappelli/', include('grappelli.urls'))"]
+    urls = Admin.urls + ["(r'^grappelli/', include('grappelli.urls'))"]
--- mason/templates/default/project_name/urls.py
+++ mason/templates/default/project_name/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-% if 'Admin' in plugin_names:
+% if 'Admin' in plugin_names or 'Django Grappelli' in plugin_names:
 from django.contrib import admin
 admin.autodiscover()
 % endif
